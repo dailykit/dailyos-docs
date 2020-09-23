e151730f1d98d07125832b3d3eb33e9af5db3854
Praveen Bisht <prvnbist@gmail.com>
2020-09-23 20:01:05 +0530

    fix: missing page

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,7 +2,7 @@ module.exports = {
    siteMetadata: {
       title: `Gatsby Docs Starter`,
       description: `A documentation starter`,
-      author: `@prvnbist`,
+      author: `@dailykit`,
       menu: [
          { title: 'Introduction', link: '/docs/introduction' },
          {
@@ -10,33 +10,60 @@ module.exports = {
             children: [
                { title: 'Overview', link: '/docs/recipe/overview' },
                { title: 'Create Recipe', link: '/docs/recipe/create' },
-               { title: 'View Recipe', link: '/docs/recipe/view' }]
+               { title: 'View Recipe', link: '/docs/recipe/view' }
+            ]
          },
          {
             title: 'Ingredient App',
             children: [
-               { title: 'Create Ingredient', link: '/docs/ingredients/create-ingredients' },
-               { title: 'View Ingredient', link: '/docs/ingredients/view-ingredients' }]
+               {
+                  title: 'Create Ingredient',
+                  link: '/docs/ingredients/create-ingredients'
+               },
+               {
+                  title: 'View Ingredient',
+                  link: '/docs/ingredients/view-ingredients'
+               }
+            ]
          },
          {
             title: 'Store App',
             children: [
                { title: 'Overview', link: '/docs/store/overview' },
                { title: 'Type of Products', link: '/docs/store/product-type' },
-               { title: 'Inventory Product', link: '/docs/store/inventory-product'},
-               { title: 'Simple Recipe Product', link: '/docs/store/simple-recipe-product'},
-               { title: 'Combo Product', link: '/docs/store/customizable-product' },
-               { title: 'Customizable Product', link: '/docs/store/combo-product' },
-               { title: 'Accompaniments', link:'/docs/store/accopaniments'},
-               { title: 'Setup Menu', link: '/docs/store/setup-menu' }]
+               {
+                  title: 'Inventory Product',
+                  link: '/docs/store/inventory-product'
+               },
+               {
+                  title: 'Simple Recipe Product',
+                  link: '/docs/store/simple-recipe-product'
+               },
+               {
+                  title: 'Combo Product',
+                  link: '/docs/store/customizable-product'
+               },
+               {
+                  title: 'Customizable Product',
+                  link: '/docs/store/combo-product'
+               },
+               // { title: 'Accompaniments', link:'/docs/store/accopaniments'},
+               { title: 'Setup Menu', link: '/docs/store/setup-menu' }
+            ]
          },
          {
             title: 'Inventory App',
             children: [
                { title: 'Overview', link: '/docs/inventory/overview' },
                { title: 'Supplier', link: '/docs/inventory/supplier' },
-               { title: 'Supplier Item', link: '/docs/inventory/supplier-item' },
-               { title: 'Purchase Order', link: '/docs/inventory/purchase-order' },
+               {
+                  title: 'Supplier Item',
+                  link: '/docs/inventory/supplier-item'
+               },
+               {
+                  title: 'Purchase Order',
+                  link: '/docs/inventory/purchase-order'
+               },
                { title: 'Work Order', link: '/docs/inventory/work-order' },
                { title: 'Packaging', link: '/docs/inventory/packaging' }
             ]
@@ -54,11 +81,15 @@ module.exports = {
             children: [
                { title: 'Overview', link: '/docs/setting/overview' },
                { title: 'Devices', link: '/docs/setting/devices' },
-               { 
-                  title: 'Master', link: '/docs/setting/master',
+               {
+                  title: 'Master',
+                  link: '/docs/setting/master',
                   children: [
-                     {title: 'Cuisines', link: '/docs/settings/cuisines'},
-                     {title: 'Processings', link: '/docs/settings/processings'},
+                     { title: 'Cuisines', link: '/docs/settings/cuisines' },
+                     {
+                        title: 'Processings',
+                        link: '/docs/settings/processings'
+                     }
                   ]
                },
                { title: 'Roles', link: '/docs/setting/roles' },
